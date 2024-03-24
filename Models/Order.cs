aca043961cfd60ec11e832cc7705b0444ef8703b
Eslam Amgad <163426064+EslamAmgad1@users.noreply.github.com>
2024-03-24 20:34:20 +0200

    Update Order.cs

--- Models/Order.cs
+++ Models/Order.cs
@@ -4,13 +4,10 @@
     {
         public int Id { get; set; }
 
-        [Required] 
         public string UserId { get; set; } = string.Empty;
 
-        [Required]
         public DateTime CreatedDate { get; set; } = DateTime.Now;
 
-        [Required]
         public int OrderStatusId { get; set; }
 
         public bool IsDeleted { get; set; } = false;
