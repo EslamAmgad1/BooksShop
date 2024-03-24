da1e07bbbc5385d9dbe5c87e2edd491044d3f0ba
Eslam Amgad <163426064+EslamAmgad1@users.noreply.github.com>
2024-03-24 20:40:02 +0200

    Update BookReposetory.cs

--- Repository/BookReposetory.cs
+++ Repository/BookReposetory.cs
@@ -8,26 +8,26 @@
             _context = context;
         }
 
-        public IEnumerable<Book> GetAllWithGenre()
+        public async Task<IEnumerable<Book>> GetAllWithGenre()
         {
-            return _context.Books.Include(b=>b.Genre).AsNoTracking().ToList();
+            return await _context.Books.Include(b=>b.Genre).AsNoTracking().ToListAsync();
         }
-        public IEnumerable<Book> GetAllByBookTitle(string title = "")
+        public async Task<IEnumerable<Book>> GetAllByBookTitle(string title = "")
         {
             title = title.ToLower();
-            return _context.Books.Include(b => b.Genre).Where(b=>b.BookName.StartsWith(title)).AsNoTracking().ToList();
+            return await _context.Books.Include(b => b.Genre).Where(b=>b.BookName.StartsWith(title)).AsNoTracking().ToListAsync();
         }
-        public IEnumerable<Book> GetAllByBookNameAndGenreId(int GenreFilter = 0, string SearchTitle = "")
+        public async Task<IEnumerable<Book>> GetAllByBookNameAndGenreId(int GenreFilter = 0, string SearchTitle = "")
         {
             SearchTitle = SearchTitle.ToLower();
             IEnumerable<Book> books;
             if (SearchTitle == "")
             {
-                books = GetAllWithGenre();
+                books = await GetAllWithGenre();
             }
             else
             {
-                books = GetAllByBookTitle(SearchTitle);
+                books = await GetAllByBookTitle(SearchTitle);
             }
             if (GenreFilter > 0)
             {
