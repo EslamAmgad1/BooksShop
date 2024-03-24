6501454ede321df08c2374930786aea5b6b207ef
Eslam Amgad <163426064+EslamAmgad1@users.noreply.github.com>
2024-03-24 20:33:46 +0200

    Update CartDetails.cs

--- Models/CartDetails.cs
+++ Models/CartDetails.cs
@@ -4,16 +4,12 @@
     {
         public int Id { get; set; }
 
-        [Required]
         public int BookId { get; set; }
 
-        [Required]
         public int Quantity { get; set; }
 
-        [Required]
         public double UnitPrice { get; set; }
 
-        [Required] 
         public int ShoppingCartId {  get; set; }
 
         public Book Book { get; set; } = default!;
